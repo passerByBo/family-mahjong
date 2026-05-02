020bb2917022555b0f12314a6fa231bfd0534e24
shixiaobo <sxb5626@163.com>
2026-05-02 22:03:04 +0800

    Add required cache and queue config to open-next

--- apps/web/open-next.config.ts
+++ apps/web/open-next.config.ts
@@ -5,6 +5,9 @@ const config: OpenNextConfig = {
     override: {
       wrapper: 'cloudflare-node',
       converter: 'edge',
+      incrementalCache: 'dummy',
+      tagCache: 'dummy',
+      queue: 'dummy',
     },
   },
 }
